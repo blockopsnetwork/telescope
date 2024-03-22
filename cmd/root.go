f2f12a35fdeaa97843881eb4a95938809fac6508
haroldsphinx <adedayoakinpelu@gmail.com>
2024-03-22 11:03:29 +0100

    Update
    Signed-off-by: haroldsphinx <adedayoakinpelu@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+Copyright © 2024 Blockops Network Inc
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
